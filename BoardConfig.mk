ab00c8f18bde59fdb814d8e8d6847d876ad9dd66
Jake Weinstein <xboxlover360@gmail.com>
2016-07-02 14:35:28 -0400

    oneplus3: cleanup boardconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,21 +41,15 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3154116608
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 59165356032
 BOARD_FLASH_BLOCK_SIZE := 262144
 
-TARGET_USES_ION := true
-TARGET_USES_NEW_ION_API :=true
-TARGET_USES_QCOM_BSP := true
-
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff
 
 BOARD_KERNEL_BASE        := 0x80000000
 BOARD_KERNEL_PAGESIZE    := 4096
-BOARD_KERNEL_TAGS_OFFSET := 0x02000000
-BOARD_RAMDISK_OFFSET     := 0x01000000
+BOARD_RAMDISK_OFFSET := 0x01000000
 
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-TARGET_USES_UNCOMPRESSED_KERNEL := false
 
 BOARD_MKBOOTIMG_ARGS := --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8996
@@ -78,42 +72,18 @@ TARGET_HW_DISK_ENCRYPTION := true
 #Enable SW based full disk encryption
 TARGET_SWV8_DISK_ENCRYPTION := false
 
-#Enable PD locater/notifier
-TARGET_PD_SERVICE_ENABLED := true
-
 BOARD_QTI_CAMERA_32BIT_ONLY := true
-TARGET_BOOTIMG_SIGNED := true
 
-# Enable dex pre-opt to speed up initial boot
-ifeq ($(HOST_OS),linux)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_PIC := true
-      ifneq ($(TARGET_BUILD_VARIANT),user)
-        # Retain classes.dex in APK's for non-user builds
-        DEX_PREOPT_DEFAULT := nostripping
-      endif
-    endif
-endif
+WITH_DEXPREOPT := true
 
 # Enable sensor multi HAL
 USE_SENSOR_MULTI_HAL := true
 
 TARGET_LDPRELOAD := libNimsWrap.so
 
-TARGET_COMPILE_WITH_MSM_KERNEL := true
-
 TARGET_KERNEL_APPEND_DTB := true
-# Added to indicate that protobuf-c is supported in this build
-PROTOBUF_SUPPORTED := false
 
 TARGET_CRYPTFS_HW_PATH := device/qcom/common/cryptfs_hw
-
-#Add support for firmare upgrade on 8996
-HAVE_SYNAPTICS_DSX_FW_UPGRADE := true
-
-# Enable MDTP (Mobile Device Theft Protection)
-TARGET_USE_MDTP := true
 
 TARGET_RECOVERY_FSTAB = device/oneplus/oneplus3/rootdir/etc/fstab.qcom
 
