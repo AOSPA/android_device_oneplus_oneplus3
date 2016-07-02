9c1a5fd7130b9a3cf1762471332cbf7d5782c76c
Jake Weinstein <xboxlover360@gmail.com>
2016-07-01 22:06:32 -0400

    oneplus3: inherit from vendor

--- oneplus3.mk
+++ oneplus3.mk
@@ -11,6 +11,7 @@ PRODUCT_COPY_FILES += device/oneplus/oneplus3/media/media_profiles.xml:system/et
                       device/oneplus/oneplus3/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
 
 $(call inherit-product, device/oneplus/oneplus3/common64.mk)
+$(call inherit-product-if-exists, vendor/oneplus/oneplus3/oneplus3-vendor.mk)
 
 PRODUCT_BOOT_JARS += tcmiface
 
