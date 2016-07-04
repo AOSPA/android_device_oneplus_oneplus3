57b61553c794147352f49fac64198fe41a791874
thecrazyskull <anaskarbila@aospa.co>
2016-07-04 16:04:39 -0400

    oneplus3: add missing mic symlink

--- Android.mk
+++ Android.mk
@@ -1,4 +1,12 @@
 #Create symbolic links
+$(shell mkdir -p $(TARGET_OUT)/etc/firmware/wcd9320; \
+    ln -sf /data/misc/audio/wcd9320_anc.bin \
+	    $(TARGET_OUT)/etc/firmware/wcd9320/wcd9320_anc.bin; \
+    ln -sf /data/misc/audio/wcd9320_mad_audio.bin \
+	    $(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_mad_audio.bin;  \
+    ln -sf /data/misc/audio/mbhc.bin \
+	    $(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_mbhc.bin)
+
 $(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/qca_cld; \
     ln -sf /system/etc/wifi/WCNSS_qcom_cfg.ini \
 	    $(TARGET_OUT)/etc/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini; \
