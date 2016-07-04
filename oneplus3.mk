6554e37446a55be366eefdcdb1a97f8dfbd17460
Alex Naidis <alex.naidis@linux.com>
2016-07-04 13:26:38 +0200

    op3: properly copy gps configs
    Change-Id: I47afb8a31284840ee2517b985846dd85b470b7d8
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- oneplus3.mk
+++ oneplus3.mk
@@ -130,14 +130,17 @@ PRODUCT_PACKAGES += power.msm8996
 PRODUCT_PACKAGES += lights.qcom
 
 # GPS
-PRODUCT_PACKAGES += \
-    gps.msm8996 \
-    flp.conf \
-    gps.conf \
-    izat.conf \
-    lowi.conf \
-    sap.conf \
-    xtwifi.conf
+
+PRODUCT_PACKAGES += gps.msm8996
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/lowi.conf:system/etc/lowi.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
+    $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
+
 
 # Doze mode
 PRODUCT_PACKAGES += \
