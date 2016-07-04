e85e4133c696d84d4759df01a95737432ec421c6
Alex Naidis <alex.naidis@linux.com>
2016-07-04 13:30:35 +0200

    op3: fix wifi symlink
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- Android.mk
+++ Android.mk
@@ -1,8 +1,11 @@
 #Create symbolic links
-$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/qca_cld; \
-        ln -sf /system/etc/wifi/WCNSS_qcom_cfg.ini \
-        $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini; \
-        ln -sf /dev/block/bootdevice/by-name/msadp \
-        $(TARGET_OUT_ETC)/firmware/msadp )
+$(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/qca_cld; \
+    ln -sf /system/etc/wifi/WCNSS_qcom_cfg.ini \
+	    $(TARGET_OUT)/etc/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini; \
+    ln -sf /persist/wlan_mac.bin \
+	    $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/wlan_mac.bin)
+
+    ln -sf /dev/block/bootdevice/by-name/msadp \
+            $(TARGET_OUT_ETC)/firmware/msadp )
 
 include $(all-subdir-makefiles)
