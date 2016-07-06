6ab8b9664e9d4efe9c6427974a9f685d0d2de1b2
Alex Naidis <alex.naidis@linux.com>
2016-07-06 16:53:18 +0200

    oneplus3: enable prediction for interactive
    * demand prediction coming from the scheduler improves performance in many cases
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -97,6 +97,7 @@ function configure_memory_parameters() {
         echo 79000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis
         echo 300000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
         echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/ignore_hispeed_on_notif
+        echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/enable_prediction
         # online CPU2
         echo 1 > /sys/devices/system/cpu/cpu2/online
         # configure governor settings for big cluster
@@ -113,6 +114,7 @@ function configure_memory_parameters() {
         echo 79000 > /sys/devices/system/cpu/cpu2/cpufreq/interactive/max_freq_hysteresis
         echo 300000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
         echo 1 > /sys/devices/system/cpu/cpu2/cpufreq/interactive/ignore_hispeed_on_notif
+        echo 1 > /sys/devices/system/cpu/cpu2/cpufreq/interactive/enable_prediction
         # re-enable thermal and BCL hotplug
         echo 1 > /sys/module/msm_thermal/core_control/enabled
         echo -n disable > /sys/devices/soc/soc:qcom,bcl/mode
