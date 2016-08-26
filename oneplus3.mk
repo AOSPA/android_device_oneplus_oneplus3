3a63ad3f7f8aa5e2e2631233eaa9031f8d14d239
Alex Naidis <alex.naidis@linux.com>
2016-08-26 16:55:27 +0200

    oneplus3: disable advanced media features
    Change-Id: I282f1f288f3a3166aa480f614af1232af647849e
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- oneplus3.mk
+++ oneplus3.mk
@@ -1,6 +1,6 @@
 DEVICE_PACKAGE_OVERLAYS := device/oneplus/oneplus3/overlay
 TARGET_USES_NQ_NFC := true
-TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+#TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 TARGET_USES_MEDIA_EXTENSIONS := true
 
 PRODUCT_BOOT_JARS += telephony-ext
@@ -115,11 +115,11 @@ PRODUCT_PACKAGES += \
     libstagefright_soft_flacenc
 
 # QC AV Enhancements
-PRODUCT_PACKAGES += \
-    libdashplayer \
-    libqcmediaplayer \
-    qcmediaplayer \
-    libextmedia_jni
+#PRODUCT_PACKAGES += \
+#    libdashplayer \
+#    libqcmediaplayer \
+#    qcmediaplayer \
+#    libextmedia_jni
 
 # SmartcardService, SIM1,SIM2,eSE1 not including eSE2,SD1 as default
 ADDITIONAL_BUILD_PROPERTIES += persist.nfc.smartcard.config=SIM1,SIM2,eSE1
