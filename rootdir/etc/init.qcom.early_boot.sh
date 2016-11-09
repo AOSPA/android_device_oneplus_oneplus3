b429640ec4d276a78a8f09c87bf36828b39f54ef
Alex Naidis <alex.naidis@linux.com>
2016-11-09 16:45:58 +0100

    oneplus3: remove power off alarm support
    * We removed platform support
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- rootdir/etc/init.qcom.early_boot.sh
+++ rootdir/etc/init.qcom.early_boot.sh
@@ -318,16 +318,3 @@ then
             set_perms $file/lineptr_value system.graphics 0664
         fi
 fi
-
-boot_reason=`cat /proc/sys/kernel/boot_reason`
-reboot_reason=`getprop ro.boot.alarmboot`
-power_off_alarm_file=`cat /persist/alarm/powerOffAlarmSet`
-if [ "$boot_reason" = "3" ] || [ "$reboot_reason" = "true" ]; then
-    if [ "$power_off_alarm_file" = "1" ]
-    then
-        setprop ro.alarm_boot true
-        setprop debug.sf.nobootanimation 1
-    fi
-else
-    setprop ro.alarm_boot false
-fi
