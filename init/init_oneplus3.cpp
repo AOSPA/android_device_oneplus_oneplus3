d92cb9977e8ceda573c4f8225fb545ee5ce0a586
Thecrazyskull <anaskarbila@gmail.com>
2017-01-14 05:54:05 -0600

    oneplus3: Setup individual battery capacities for 3/3T
    * Remove battery capacity from power profile
    * Use the propertly we introduced to set the
      battery capacity from our init extension
      at runtime.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- init/init_oneplus3.cpp
+++ init/init_oneplus3.cpp
@@ -38,8 +38,11 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
 
     std::string project_name = property_get("ro.boot.project_name");
 
-    if (project_name == "15811")
+    if (project_name == "15811") {
         property_set("ro.product.model", "OnePlus 3T");
-    else
+        property_set("ro.battery.capacity", "3400");
+    } else {
         property_set("ro.product.model", "OnePlus 3");
+        property_set("ro.battery.capacity", "3000");
+    }
 }
