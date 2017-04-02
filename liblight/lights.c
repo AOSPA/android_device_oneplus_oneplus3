19fbde2b892056209b078f8c00ec72e5f9149814
Alex Naidis <alex.naidis@linux.com>
2017-04-02 15:28:25 +0200

    oneplus3: Fix liblight copyright
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- liblight/lights.c
+++ liblight/lights.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
  * Copyright (C) 2014 The Linux Foundation. All rights reserved.
- * Copyright (C) 2016 Paranoid Android
+ * Copyright (C) 2015 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
