24edec2621f3a946638fc38f7ae5f54bf2c19abc
Ramjee Singh <ramjee@codeaurora.org>
2017-12-28 14:55:43 +0530

    msm8996: Move flac decoder to vendor specific xml file
    - Moved FlacDecoder to a new vendor specific xml file
      to avoid VTS test case failures on vanilla system
      image.
    
    Change-Id: I1588b39780ab85becbbabc214f6d1bf88d7cb69e

--- msm8996.mk
+++ msm8996.mk
@@ -54,7 +54,8 @@ PRODUCT_COPY_FILES += \
     device/qcom/msm8996/media_profiles.xml:system/etc/media_profiles.xml \
     device/qcom/msm8996/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
     device/qcom/msm8996/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
-    device/qcom/msm8996/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
+    device/qcom/msm8996/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
+    device/qcom/msm8996/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml
 endif  #TARGET_ENABLE_QC_AV_ENHANCEMENTS
 
 PRODUCT_COPY_FILES += device/qcom/msm8996/whitelistedapps.xml:system/etc/whitelistedapps.xml \
