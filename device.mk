f5f0d23d16bac8b03efa167b13e8cd070c120ba7
Alexander Koskovich <zvnexus@outlook.com>
2020-03-18 04:00:20 -0400

    op3: Build libaudioroute.
    Change-Id: If10774e05dfde6b73899f5b84f48e63e1e4ff7a3

--- device.mk
+++ device.mk
@@ -124,6 +124,7 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
+    libaudioroute \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
