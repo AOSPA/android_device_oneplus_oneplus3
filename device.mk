3a7f99518f4e14e7ada57b9a3d64d94ddb06c8c3
Alexander Koskovich <zvnexus@outlook.com>
2020-03-18 02:55:58 -0400

    op3: Move GPS configurations to vendor.
    Change-Id: I47b116d39b5451c88a503b56aacb99d81e4281ec

--- device.mk
+++ device.mk
@@ -286,14 +286,6 @@ PRODUCT_PACKAGES += \
     libsensorndkbridge \
     libwifi-hal-ctrl
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
-    $(LOCAL_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
-    $(LOCAL_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
-    $(LOCAL_PATH)/gps/etc/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
-    $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
-    $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
-
 # Healthd
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
