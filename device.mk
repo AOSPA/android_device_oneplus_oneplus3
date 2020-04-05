a06b54887351c092474312e58dc42e9c16bb21d6
Alexander Koskovich <zvnexus@outlook.com>
2020-04-05 16:48:40 -0400

    op3: Build the v1.2 radio configuration.
     * We don't actually use it but RIL references it,
       so just include it.
    
    android.hardware.radio.config@1.1.so required by:
        libril-qc-hal-qmi.so;
    
    android.hardware.radio.config@1.2.so required by:
        libril-qc-hal-qmi.so;
    
    Change-Id: I1240af33d2265bdf7043f6693ffdc427251363c6

--- device.mk
+++ device.mk
@@ -452,7 +452,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
-    android.hardware.radio.config@1.0 \
+    android.hardware.radio.config@1.2 \
     android.hardware.secure_element@1.0 \
     librmnetctl \
     libxml2 \
