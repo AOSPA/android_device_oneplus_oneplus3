f8f3d5930507bfb6227fcf43c4d1dc3a22f1a917
Alexander Koskovich <zvnexus@outlook.com>
2020-04-21 13:59:10 -0400

    op3: Use the thermal stack from OOS 9.0.6.
    Change-Id: I7d9efa0b557f45b73ea1797d09d58c43b840792a

--- device.mk
+++ device.mk
@@ -490,12 +490,8 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    thermal.msm8996 \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 # USB
 PRODUCT_PACKAGES += \
