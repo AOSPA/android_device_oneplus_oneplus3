ffe48893ffacbe46f640f1a18279bd3d9c1f6d95
Alexander Koskovich <zvnexus@outlook.com>
2020-04-22 17:30:45 -0400

    op3: Move some radio packages to QCOM common.
    Change-Id: I63a2d1df46747ec821469ce95a21054db7881b72

--- device.mk
+++ device.mk
@@ -463,8 +463,6 @@ TARGET_COMMON_QTI_COMPONENTS := \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.4 \
-    android.hardware.radio.config@1.2 \
     android.hardware.secure_element@1.0 \
     librmnetctl \
     libxml2 \
