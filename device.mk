cc720c993a5848531b10fc45083276cd6f1ce0e8
Jake Weinstein <jake@aospa.co>
2020-07-07 06:37:27 -0400

    op3: Move power HAL definitions out of tree.
    Change-Id: I724f25902493282cde6c8e942f7dde1cb23d7f0f

--- device.mk
+++ device.mk
@@ -325,9 +325,6 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
